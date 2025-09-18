c62dffaf540a46b2d2187a95194c7d593c4a7a02
Sean Outhwaite <sean.outhwaite@gmail.com>
2025-09-19 11:40:35 +1200

    Attempt to add sound

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,10 +1,18 @@
 'use strict'
+import * as Tone from 'https://esm.run/tone'
+
+const honkNoise = new Tone.Oscillator({
+  type: 'square',
+  frequency: 150,
+  volume: -16,
+}).toDestination()
+
 document.getElementById('collapsible').addEventListener('click', function () {
   document
     .querySelector('.collapse-section')
     .scrollIntoView({ behavior: 'smooth' })
 })
-
+honkNoise.start()
 let beaks = document.querySelectorAll('.honk')
 function honk() {
   for (let i = 0; i < beaks.length; i++) {
@@ -25,4 +33,5 @@ window.addEventListener('mouseup', function () {
   beaks[0].classList.remove('hidden')
   beaks[1].classList.add('hidden')
   beaks[2].classList.add('hidden')
+  honkNoise.stop()
 })
