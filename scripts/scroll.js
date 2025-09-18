828eb32db359f52b686907a17f58619532684fd3
Sean Outhwaite <sean.outhwaite@gmail.com>
2025-09-19 11:30:35 +1200

    Make honk function only run while mouse button is held down on swan element

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -5,11 +5,24 @@ document.getElementById('collapsible').addEventListener('click', function () {
     .scrollIntoView({ behavior: 'smooth' })
 })
 
+let beaks = document.querySelectorAll('.honk')
 function honk() {
-  let beaks = document.querySelectorAll('.honk')
   for (let i = 0; i < beaks.length; i++) {
     beaks[i].classList.toggle('hidden')
   }
 }
 
-document.querySelector('.swan').addEventListener('click', honk)
+document.querySelector('.swan').addEventListener('mousedown', honk)
+
+// document.addEventListener('mouseup', function () {
+//   if ()
+//   for (let i = 0; i < beaks.length; i++) {
+//       beaks[i].classList.toggle('hidden')
+//     }
+//   })
+
+window.addEventListener('mouseup', function () {
+  beaks[0].classList.remove('hidden')
+  beaks[1].classList.add('hidden')
+  beaks[2].classList.add('hidden')
+})
