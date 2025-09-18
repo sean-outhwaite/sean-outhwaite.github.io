912fc987b6deac84bec67b2f6362ce07bbe8b5eb
Sean Outhwaite <sean.outhwaite@gmail.com>
2025-09-19 10:57:43 +1200

    Add honk feature

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -4,3 +4,12 @@ document.getElementById('collapsible').addEventListener('click', function () {
     .querySelector('.collapse-section')
     .scrollIntoView({ behavior: 'smooth' })
 })
+
+function honk() {
+  let beaks = document.querySelectorAll('.honk')
+  for (let i = 0; i < beaks.length; i++) {
+    beaks[i].classList.toggle('hidden')
+  }
+}
+
+document.querySelector('.swan').addEventListener('click', honk)
