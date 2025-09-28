6ef24e17c172e94b97d80871c83737e869d24d18
Sean Outhwaite <sean.outhwaite@gmail.com>
2025-09-29 11:07:36 +1300

    Add HTML head and footer to the output content

--- scripts/bloggen.js
+++ scripts/bloggen.js
@@ -9,9 +9,40 @@ const bottomPreview = document.querySelector('.bottomPreview')
 const preview = document.querySelector('.preview')
 
 let blog = {
+  head: `<!DOCTYPE html>
+<html>
+  <head>
+    <title>placeholder</title>
+    <meta charset="UTF-8" />
+    <link
+      rel="icon"
+      href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🦢</text></svg>"
+    />
+    <link href="../styles/main.css" rel="stylesheet" type="text/css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+      rel="stylesheet"
+    />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  </head>
+
+  <body>
+  <a id="homeAnchor" href="../index.html"> <button id="home">Home</button></a>
+    <main class="container">`,
   title: '',
   subHead: '',
   blogDate: '',
+  foot: `</main>
+    <footer>
+      <a href="https://github.com/sean-outhwaite"
+        ><img class="gitLogo" src="/images/github-mark.png"
+      /></a>
+    </footer>
+    <script type="module" src="/scripts/script.js"></script>
+  </body>
+</html>`,
 }
 
 document.getElementById('headAdd').addEventListener('click', addHeading)
@@ -50,7 +81,8 @@ function addDate() {
 
 function generateTop() {
   let top = blog.title + blog.subHead + blog.blogDate
-  document.querySelector('.topContent').textContent = top
+  document.querySelector('.topContent').textContent = blog.head + top
+  document.querySelector('.footerContent').textContent = blog.foot
   document.querySelector('.topPreview').innerHTML = top
   preview.classList.remove('hidden')
 }
